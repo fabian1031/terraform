b94821f4a07807824aabb967f6ca9ac8e9f51879
fabian1031 <lugia759@gmail.com>
2026-06-18 14:03:54 -0500

    creado maquina virtual en ec2

--- main.tf
+++ main.tf
@@ -9,7 +9,24 @@ terraform {
 provider "aws"{
     region = "us-east-1"
 }
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
 //creo clase y mi instancia
 resource "aws_s3_bucket" "mi_bucket" {
     bucket = var.nombre_bucket //llamo a mi variable
+}
+resource "aws_instance" "mi_servidor" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mi-primer-servidor"
+  }
 }
