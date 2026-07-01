4d20e80f0246fcb736db8bce006963cbe07d75a0
fabian1031 <lugia759@gmail.com>
2026-07-01 11:10:24 -0500

    correccion de variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "nombre_bucket" {
-    description = "Nombre del bucket s3" //documenta
-    type = string
-    default = "mi-primer-bucket-terraform-123"
-  
+  description = "Nombre del bucket s3" //documenta
+  type        = string
+  default     = "mi-primer-bucket-terraform-123"
+
 }
